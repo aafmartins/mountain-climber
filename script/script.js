c9a42a370b26b9ba2e83216a5face5d04d84f877
Ana Martins <ana.af.martins@gmail.com>
2021-08-22 16:23:25 +0200

    Basic html and css styling added + notes to script

---
+++ script/script.js
@@ -0,0 +1,14 @@
+// On window load, addeventlistener 'click' to start, example below
+
+console.log("My game has started");
+
+/*window.onload = function () {
+  document.getElementById("start-button").onclick = function () {
+    startGame();
+  };
+
+  function startGame() {
+    const myGame = new Game();
+    myGame.init();
+  }
+};*/
