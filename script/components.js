d5431e68c23511ff55037cff88f74ada71ef92aa
Ana Martins <ana.af.martins@gmail.com>
2021-08-23 12:13:06 +0200

    Game components factory built

---
+++ script/components.js
@@ -0,0 +1,15 @@
+// Build game components factory
+
+class Components {
+  constructor(canvasContext, image, positionX, positionY, width, height) {
+    this.ctx = canvasContext;
+    this.img = image;
+    this.x = positionX;
+    this.y = positionY;
+    this.width = width;
+    this.height = height;
+  }
+  draw() {
+    this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+}
